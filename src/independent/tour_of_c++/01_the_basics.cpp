602478d595e732b3aa220ff67e230c394dfb41d9
ijs32 <iansilber32@gmail.com>
2025-09-05 13:41:33 -0500

    sync

--- src/independent/tour_of_c++/01_the_basics.cpp
+++ src/independent/tour_of_c++/01_the_basics.cpp
@@ -1,3 +1,48 @@
 #include <iostream>
+#include <vector>
 
-int main() {}
+using namespace std;
+
+double square(int x)
+{
+  return x * x;
+}
+
+int main()
+{
+  int x = 4;
+  int y = 2;
+
+  cout << "The square of " << x << " is " << square(x);
+
+  x += y; // x = x+y
+  ++x;    // increment: x = x+1
+  x -= y; // x = x-y
+  --x;    // decrement: x = x-1
+  x *= y; // scaling: x = x*y
+  x /= y; // scaling: x = x/y
+  x %= y; // x = x%y
+}
+
+// function overloading, you can have multiple functions of the same name but accepting different types
+void print(int x);
+void print(double x);
+void print(string x);
+
+void user()
+{
+  print(42);
+  print(42.53);
+  print("D is for Digital");
+}
+
+vector<int> vec = {1, 2, 3, 4, 5};
+
+// we can let the initizializer choose a variable type for us
+auto b = true; // a bool
+auto ch = 'x'; // a char
+auto i = 123;  // an int
+auto d = 1.2;  // a double
+auto y = 2;
+auto z = sqrt(y); // z has the type of whatever sqrt(y) returns
+auto x = 1;
